92f029f09c9942ef471dc0b3b87dc8ff418a8120
Tu nombre <tu@ejemplo.com>
2024-11-10 13:08:13 +0100

    16

--- blog/data/modelo/ciudades.py
+++
@@ -1,6 +0,0 @@
-class Ciudad:
-    def __init__(self, id : int, ciudades : str, contenido : str, id_paises : str):
-        self.id = id
-        self.ciudades = ciudades
-        self.contenido = contenido
-        self.id_paises = id_paises
--- blog/data/modelo/paises.py
+++
@@ -1,4 +0,0 @@
-class Pais:
-    def __init__(self, id : int, nombre : str):
-        self.id = id
-        self.nombre = nombre
